d972089680d9652a71cf0d2670a1daa47eaa0afe
exreaction@gmail.com <exreaction@gmail.com@968d5c96-4656-0410-87ea-bbcc4c491358>
2008-10-21 17:54:49 +0000

    Updating remove_asacp.php
    git-svn-id: http://antispam-acp.googlecode.com/svn/trunk@80 968d5c96-4656-0410-87ea-bbcc4c491358

--- root/antispam/remove_asacp.php
+++ root/antispam/remove_asacp.php
@@ -40,17 +40,48 @@ if ($umif->confirm_box(true))
 {
 	$umif->display_stages(array('CONFIRM', 'UNINSTALL'), 2);
 
+	// Remove the Modules
+	$umif->module_remove('acp', 'ASACP_SETTINGS');
+	$umif->display_results();
+	$umif->module_remove('acp', 'ASACP_SPAM_LOG');
+	$umif->display_results();
+	$umif->module_remove('acp', 'ASACP_FLAG_LOG');
+	$umif->display_results();
+	$umif->module_remove('acp', 'ASACP_FLAG_LIST');
+	$umif->display_results();
+	$umif->module_remove('acp', 'ASACP_IP_SEARCH');
+	$umif->display_results();
+	$umif->module_remove('acp', 'ASACP_SPAM_WORDS');
+	$umif->display_results();
+	$umif->module_remove('acp', 'ASACP_PROFILE_FIELDS');
+	$umif->display_results();
+	$umif->module_remove('acp', 'ANTISPAM');
+	$umif->display_results();
+
+
+	// 0.3.4
+	$umif->table_column_remove(USERS_TABLE, 'user_flag_new');
+	$umif->display_results();
+	$umif->config_remove('asacp_notify_new_flag');
+	$umif->display_results();
+	$umif->config_remove('asacp_user_flag_enable');
+	$umif->display_results();
+
+	// 0.3.2
 	$umif->table_remove(SPAM_LOG_TABLE);
 	$umif->display_results();
 
+	// 0.3.1
 	$umif->table_column_remove(USERS_TABLE, 'user_flagged');
 	$umif->display_results();
 
+	// 0.1.11
 	$umif->config_remove('asacp_profile_signature');
 	$umif->display_results();
 	$umif->config_remove('asacp_profile_signature_post_limit');
 	$umif->display_results();
 
+	// 0.1.10
 	$umif->config_remove('asacp_profile_icq');
 	$umif->display_results();
 	$umif->config_remove('asacp_profile_icq_post_limit');
@@ -88,55 +119,49 @@ if ($umif->confirm_box(true))
 	$umif->config_remove('asacp_profile_interests_post_limit');
 	$umif->display_results();
 
+	// 0.1.9
 	$umif->config_remove('asacp_spam_words_pm_action');
 	$umif->display_results();
 
+	// 0.1.8
 	$umif->config_remove('asacp_spam_words_profile_action');
 	$umif->display_results();
 
+	// 0.1.7
 	$umif->config_remove('asacp_spam_words_posting_action');
 	$umif->display_results();
 
+	// 0.1.6
 	$umif->config_remove('asacp_spam_words_flag_limit');
 	$umif->display_results();
 
+	// 0.1.5
 	$umif->config_remove('asacp_spam_words_enable');
 	$umif->display_results();
 	$umif->config_remove('asacp_spam_words_post_limit');
 	$umif->display_results();
 
+	// 0.1.4
 	$umif->table_remove(SPAM_WORDS_TABLE);
 	$umif->display_results();
 
+	// 0.1.3
 	$umif->config_remove('asacp_log');
 	$umif->display_results();
 
+	// 0.1.2
 	$umif->config_remove('asacp_reg_captcha');
 	$umif->display_results();
 
+	// 0.1.1
 	$umif->permission_remove('a_asacp', 'global');
 	$umif->display_results();
 
+	// 0.1.0
 	$umif->config_remove('asacp_enable');
 	$umif->display_results();
 
 	$umif->config_remove('asacp_version');
-	$umif->display_results();
-
-	// Remove the Modules
-	$umif->module_remove('acp', 'ASACP_SETTINGS');
-	$umif->display_results();
-	$umif->module_remove('acp', 'ASACP_SPAM_LOG');
-	$umif->display_results();
-	$umif->module_remove('acp', 'ASACP_FLAG_LOG');
-	$umif->display_results();
-	$umif->module_remove('acp', 'ASACP_IP_SEARCH');
-	$umif->display_results();
-	$umif->module_remove('acp', 'ASACP_SPAM_WORDS');
-	$umif->display_results();
-	$umif->module_remove('acp', 'ASACP_PROFILE_FIELDS');
-	$umif->display_results();
-	$umif->module_remove('acp', 'ANTISPAM');
 	$umif->display_results();
 
 	$umif->purge_cache();
