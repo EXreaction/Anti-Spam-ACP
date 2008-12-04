75d79d70eaf5f43f38dd21900a7a906f03c3cd48
exreaction@gmail.com <exreaction@gmail.com@968d5c96-4656-0410-87ea-bbcc4c491358>
2008-12-04 21:27:57 +0000

    Moving functions required for updating/installing/uninstalling to versions file.
    git-svn-id: http://antispam-acp.googlecode.com/svn/trunk@110 968d5c96-4656-0410-87ea-bbcc4c491358

--- root/antispam/asacp_versions.php
+++ root/antispam/asacp_versions.php
@@ -169,4 +169,45 @@ $versions = array(
 	'0.7.2'		=> array(),
 );
 
+function asacp_update($action, $version)
+{
+	global $db, $umil;
+
+	if ($action != 'update')
+	{
+		return;
+	}
+
+	switch ($version)
+	{
+		case '0.3.2' :
+			// Moving the Spam log from the Log table to the Spam Log table.
+			$sql = 'SELECT * FROM ' . LOG_TABLE . ' WHERE log_type = ' . LOG_SPAM;
+			$result = $db->sql_query($sql);
+			$insert_ary = array();
+			while ($row = $db->sql_fetchrow($result))
+			{
+				unset($row['log_id']);
+
+				$row['log_type'] = 1;
+				$insert_ary[] = $row;
+			}
+			$db->sql_freeresult($result);
+
+			$db->sql_multi_insert(SPAM_LOG_TABLE, $insert_ary);
+
+			$db->sql_query('DELETE FROM ' . LOG_TABLE . ' WHERE log_type = ' . LOG_SPAM);
+		break;
+
+		case '0.7.0' :
+			// Permissions changed, re-adding modules
+			$umil->module_remove('acp', 'ANTISPAM', array('module_basename' => 'asacp'));
+			$umil->module_remove('acp', 'ACP_CAT_DOT_MODS', 'ANTISPAM');
+
+			$umil->module_add('acp', 'ANTISPAM', array('module_basename' => 'asacp'));
+			$umil->module_add('acp', 'ACP_CAT_DOT_MODS', 'ANTISPAM');
+		break;
+	}
+}
+
 ?>
