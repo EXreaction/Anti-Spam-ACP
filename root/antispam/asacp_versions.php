5b02ac866c0c729edb4e3d2cef3ad38d7eac0ba6
exreaction@gmail.com <exreaction@gmail.com@968d5c96-4656-0410-87ea-bbcc4c491358>
2009-01-13 18:37:58 +0000

    0.9.0
    git-svn-id: http://antispam-acp.googlecode.com/svn/trunk@120 968d5c96-4656-0410-87ea-bbcc4c491358

--- root/antispam/asacp_versions.php
+++ root/antispam/asacp_versions.php
@@ -168,7 +168,7 @@ $versions = array(
 	'0.7.1'		=> array(),
 	'0.9.0'		=> array(
 		'config_add'	=> array(
-			array('asacp_sfs_action', 1),
+			array('asacp_sfs_action', 2),
 			array('asacp_sfs_min_freq', 2),
 			array('asacp_sfs_key', ''),
 			array('asacp_ocban_username', true),
